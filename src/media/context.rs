5180a7e237d813eb23f24f0455c5591d13b4686a
fengalin <fengalin@free.fr>
2017-07-10 01:30:21 +0200

    Add more data (#1)

--- src/media/context.rs
+++ src/media/context.rs
@@ -35,10 +35,9 @@ impl Context {
                     new_ctx.description = String::from(format.description());
 
                     let stream_iter = new_ctx.ffmpeg_context.streams();
-                    stream_count = stream_iter.size_hint().0;
-
                     new_ctx.video_stream = VideoStream::new(&stream_iter);
                     new_ctx.audio_stream = AudioStream::new(&stream_iter);
+                    stream_count = stream_iter.size_hint().0;
                 }
 
                 // TODO: process metadata
@@ -65,6 +64,7 @@ impl Context {
 
 }
 
+// TODO: use lifetimes to hold ffmped stream and codec within the structure below
 pub struct Stream {
     pub index: usize,
     pub time_base: Rational,
@@ -73,46 +73,111 @@ pub struct Stream {
     pub frames: i64,
     pub disposition: Disposition,
     pub rate: Rational,
+    pub discard: ffmpeg::Discard,
     pub avg_frame_rate: Rational,
+    pub codec_medium: ffmpeg::media::Type,
+    pub codec_id: ffmpeg::codec::Id,
 }
 
 impl Stream {
-    pub fn new(stream_iter: &ffmpeg::format::context::common::StreamIter,
-               media_type: ffmpeg::media::Type) -> Option<Stream> {
-        let stream = stream_iter.best(media_type);
-        match stream {
-            Some(stream) => {
-                Some(Stream {
-                    index: stream.index(),
-                    time_base: stream.time_base(),
-                    start_time: stream.start_time(),
-                    duration: stream.duration(),
-                    frames: stream.frames(),
-                    disposition: stream.disposition(),
-                    rate: stream.rate(),
-                    avg_frame_rate: stream.avg_frame_rate(),
-                })
-            },
-            None => None,
-        }
+    pub fn new(stream: ffmpeg::format::stream::Stream) -> Stream {
+        let codec = stream.codec();
+        let new_stream = Stream {
+            index: stream.index(),
+            time_base: stream.time_base(),
+            start_time: stream.start_time(),
+            duration: stream.duration(),
+            frames: stream.frames(),
+            disposition: stream.disposition(),
+            discard: stream.discard(),
+            rate: stream.rate(),
+            avg_frame_rate: stream.avg_frame_rate(),
+            codec_medium: codec.medium(),
+            codec_id: codec.id(),
+        };
+
+        println!("stream index {} - {:?}:", new_stream.index, new_stream.codec_medium);
+        println!("\ttime_base: {}", new_stream.time_base);
+        println!("\tstart_time: {}", new_stream.start_time);
+        println!("\tduration (stream timebase): {}", new_stream.duration);
+        println!("\tduration (seconds): {:.2}", new_stream.duration as f64 * f64::from(stream.time_base()));
+        println!("\tframes: {}", new_stream.frames);
+        println!("\tdisposition: {:?}", new_stream.disposition);
+        println!("\tdiscard: {:?}", new_stream.discard);
+        println!("\trate: {}", new_stream.rate);
+        println!("\tcodec_id: {:?}", new_stream.codec_id);
+
+        new_stream
     }
 }
 
 pub struct VideoStream {
     pub stream: Stream,
+	pub bit_rate: usize,
+	pub max_bit_rate: usize,
+	pub delay: usize,
+    width: u32,
+    height: u32,
+    format: ffmpeg::format::Pixel,
+    has_b_frames: bool,
+    aspect_ratio: ffmpeg::Rational,
+    color_space: ffmpeg::color::Space,
+    color_range: ffmpeg::color::Range,
+    color_primaries: ffmpeg::color::Primaries,
+    color_transfer_characteristic: ffmpeg::color::TransferCharacteristic,
+    chroma_location: ffmpeg::chroma::Location,
+    references: usize,
+    intra_dc_precision: u8,
 }
 
 impl VideoStream {
     pub fn new(stream_iter: &ffmpeg::format::context::common::StreamIter) -> Option<VideoStream> {
-        let video_stream = Stream::new(stream_iter, ffmpeg::media::Type::Video);
-
-        match video_stream {
+        match stream_iter.best(ffmpeg::media::Type::Video) {
             Some(video_stream) => {
-                if video_stream.frames > 0 {
-                    Some(VideoStream{ stream: video_stream, })
-                }
-                else {
-                    None
+                match video_stream.codec().decoder().video() {
+                    Ok(video) => {
+                        let new_vs = VideoStream{
+                            bit_rate: video.bit_rate(),
+                            max_bit_rate: video.max_bit_rate(),
+                            delay: video.delay(),
+                            width: video.width(),
+                            height: video.height(),
+                            format: video.format(),
+                            has_b_frames: video.has_b_frames(),
+                            aspect_ratio: video.aspect_ratio(),
+                            color_space: video.color_space(),
+                            color_range: video.color_range(),
+                            color_primaries: video.color_primaries(),
+                            color_transfer_characteristic: video.color_transfer_characteristic(),
+                            chroma_location: video.chroma_location(),
+                            references: video.references(),
+                            intra_dc_precision: video.intra_dc_precision(),
+                            stream: Stream::new(video_stream),
+                        };
+
+                        println!("\tbit_rate: {}", new_vs.bit_rate);
+                        println!("\tmax_bit_rate: {}", new_vs.max_bit_rate);
+                        println!("\tdelay: {}", new_vs.delay);
+					    println!("\twidth: {}", new_vs.width);
+					    println!("\theight: {}", new_vs.height);
+					    println!("\tformat: {:?}", new_vs.format);
+					    println!("\thas_b_frames: {}", new_vs.has_b_frames);
+					    println!("\taspect_ratio: {}", new_vs.aspect_ratio);
+					    println!("\tcolor_space: {:?}", new_vs.color_space);
+					    println!("\tcolor_range: {:?}", new_vs.color_range);
+					    println!("\tcolor_primaries: {:?}", new_vs.color_primaries);
+					    println!("\tcolor_transfer_characteristic: {:?}", new_vs.color_transfer_characteristic);
+					    println!("\tchroma_location: {:?}", new_vs.chroma_location);
+					    println!("\treferences: {}", new_vs.references);
+                        println!("\tintra_dc_precision: {}", new_vs.intra_dc_precision);
+
+                        Some(new_vs)
+                    },
+                    Err(error) => {
+                        println!("audio stream: {:?}", error);
+                        // TODO: should probably panic here
+                        None
+                    }
                 }
             },
             None => None,
@@ -138,19 +203,56 @@ impl DerefMut for VideoStream {
 
 pub struct AudioStream {
     pub stream: Stream,
+	pub bit_rate: usize,
+	pub max_bit_rate: usize,
+	pub delay: usize,
+	pub rate: u32,
+	pub channels: u16,
+	pub format: ffmpeg::format::Sample,
+	pub frames: usize,
+	pub align: usize,
+	pub channel_layout: ffmpeg::ChannelLayout,
+    pub frame_start: Option<usize>,
 }
 
 impl AudioStream {
     pub fn new(stream_iter: &ffmpeg::format::context::common::StreamIter) -> Option<AudioStream> {
-        let audio_stream = Stream::new(stream_iter, ffmpeg::media::Type::Audio);
-
-        match audio_stream {
+        match stream_iter.best(ffmpeg::media::Type::Audio) {
             Some(audio_stream) => {
-                if audio_stream.frames > 0 || audio_stream.duration > 0 {
-                    Some(AudioStream{ stream: audio_stream, })
-                }
-                else {
-                    None
+                match audio_stream.codec().decoder().audio() {
+                    Ok(audio) => {
+                        let new_as = AudioStream{
+                            bit_rate: audio.bit_rate(),
+                            max_bit_rate: audio.max_bit_rate(),
+                            delay: audio.delay(),
+                            rate: audio.rate(),
+                            channels: audio.channels(),
+                            format: audio.format(),
+                            frames: audio.frames(),
+                            align: audio.align(),
+                            channel_layout: audio.channel_layout(),
+                            frame_start: audio.frame_start(),
+                            stream: Stream::new(audio_stream),
+                        };
+
+                        println!("\tbit_rate: {}", new_as.bit_rate);
+                        println!("\tmax_bit_rate: {}", new_as.max_bit_rate);
+                        println!("\tdelay: {}", new_as.delay);
+                        println!("\trate: {}", new_as.rate);
+                        println!("\tchannels: {}", new_as.channels);
+                        println!("\tformat: {:?}", new_as.format);
+                        println!("\tframes: {}", new_as.frames);
+                        println!("\talign: {}", new_as.align);
+                        println!("\tchannel_layout: {:?}", new_as.channel_layout);
+                        println!("\tframe_start: {:?}", new_as.frame_start);
+
+                        Some(new_as)
+                    },
+                    Err(error) => {
+                        println!("audio stream: {:?}", error);
+                        // TODO: should probably panic here
+                        None
+                    }
                 }
             },
             None => None,
