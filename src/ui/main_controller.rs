98d1a1c6544cbad289aabaa8100b5a487828c2a8
fengalin <fengalin@free.fr>
2017-09-24 14:45:05 +0200

    Fix profiling (#24)

--- src/ui/main_controller.rs
+++ src/ui/main_controller.rs
@@ -261,7 +261,7 @@ impl MainController {
             let end = Utc::now();
 
             #[cfg(feature = "profiling-tracker")]
-            println!("tracker,{},{},{},{},{}",
+            println!("tracker,{},{},{},{}",
                 start.time().format("%H:%M:%S%.6f"),
                 before_tic.time().format("%H:%M:%S%.6f"),
                 end.time().format("%H:%M:%S%.6f"),
