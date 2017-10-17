4fa8569d9277a4fe8b9ff493e7923f81a10e1c28
fengalin <fengalin@free.fr>
2017-10-17 18:09:08 +0200

    AudioBuffer: add a feature to trace occasional cases (#58)

--- src/media/audio_buffer.rs
+++ src/media/audio_buffer.rs
@@ -176,8 +176,10 @@ impl AudioBuffer {
                 } else if incoming_lower >= self.lower
                 && incoming_upper <= self.upper {
                     // 2. incoming buffer included in current container
-                    #[cfg(test)]
-                    println!("AudioBuffer case 2. contained in current container");
+                    #[cfg(any(test, feature = "trace-audio_buffer"))]
+                    println!("AudioBuffer case 2. contained in current container self [{}, {}], incoming [{}, {}]",
+                        self.lower, self.upper, incoming_lower, incoming_upper
+                    );
                     self.last_buffer_upper = incoming_upper;
                     (
                         false,           // lower_changed
@@ -188,8 +190,10 @@ impl AudioBuffer {
                 } else if incoming_lower > self.lower
                 && incoming_lower < self.upper
                 {   // 3. can append [self.upper, upper] to the end
-                    #[cfg(test)]
-                    println!("AudioBuffer case 3. append to the end (partial)");
+                    #[cfg(any(test, feature = "trace-audio_buffer"))]
+                    println!("AudioBuffer case 3. append to the end (partial) [{}, {}], incoming [{}, {}]",
+                        self.upper, incoming_upper, incoming_lower, incoming_upper
+                    );
                     // self.lower unchanged
                     let previous_upper = self.upper;
                     self.upper = incoming_upper;
@@ -205,8 +209,10 @@ impl AudioBuffer {
                 else if incoming_upper < self.upper
                 && incoming_upper >= self.lower
                 {   // 4. can insert [lower, self.lower] at the begining
-                    #[cfg(test)]
-                    println!("AudioBuffer case 4. insert at the begining");
+                    #[cfg(any(test, feature = "trace-audio_buffer"))]
+                    println!("AudioBuffer case 4. insert at the begining [{}, {}], incoming [{}, {}]",
+                        incoming_lower, self.lower, incoming_lower, incoming_upper
+                    );
                     let upper_to_add = self.lower;
                     self.lower = incoming_lower;
                     // self.upper unchanged
@@ -219,7 +225,7 @@ impl AudioBuffer {
                     )
                 } else {
                     // 5. can't merge with previous buffer
-                    #[cfg(test)]
+                    #[cfg(any(test, feature = "trace-audio_buffer"))]
                     println!("AudioBuffer case 5. can't merge self [{}, {}], incoming [{}, {}]",
                         self.lower, self.upper, incoming_lower, incoming_upper
                     );
