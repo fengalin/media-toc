e70e7121c231b92a83225f13b920b898016d249a
François Laignel <fengalin@free.fr>
2018-12-08 17:52:02 +0100

    CI: switch back to stable & use recent gstreamer version (#126)

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_OS_NAME = linux ]; then
     # Trusty uses pretty old versions => use newer
 
     # GStreamer
-    curl -L https://people.freedesktop.org/~slomo/gstreamer.tar.gz | tar xz
+    curl -L https://people.freedesktop.org/~slomo/gstreamer-1.14.3.tar.gz | tar xz
     sed -i "s;prefix=/root/gstreamer;prefix=$PWD/gstreamer;g" $PWD/gstreamer/lib/x86_64-linux-gnu/pkgconfig/*.pc
     export PKG_CONFIG_PATH=$PWD/gstreamer/lib/x86_64-linux-gnu/pkgconfig
     export LD_LIBRARY_PATH=$PWD/gstreamer/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
