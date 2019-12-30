3d5ba47d095517f6cc69ce6bcf87d4fd2145840a
François Laignel <fengalin@free.fr>
2019-12-30 19:53:57 +0100

    Update po files + fix translations no longer detected

--- application/src/locale.rs
+++ application/src/locale.rs
@@ -2,17 +2,17 @@ use directories::ProjectDirs;
 use gettextrs::{TextDomain, TextDomainError};
 use log::{error, info, warn};
 
-use super::{SLD, TLD};
+use super::{APP_NAME, SLD, TLD};
 
 pub fn init_locale() {
     // Search translations under `target` first
     // in order to reflect latest changes during development
-    let text_domain = TextDomain::new(env!("CARGO_PKG_NAME"))
+    let text_domain = TextDomain::new(&*APP_NAME)
         .codeset("UTF-8")
         .prepend("target");
 
     // Add user's data dir in the search path
-    let project_dirs = ProjectDirs::from(TLD, SLD, env!("CARGO_PKG_NAME"))
+    let project_dirs = ProjectDirs::from(TLD, SLD, &APP_NAME)
         .expect("Couldn't find project dirs for this platform");
     let _app_data_dir = project_dirs.data_dir();
 
